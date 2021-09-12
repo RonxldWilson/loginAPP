3bdee0f75b9e3145524a73527cc412c6de62c5ad
RonxldWilson <57818133+RonxldWilson@users.noreply.github.com>
2021-09-12 15:51:01 +0530

    New Changes Added Implicit Intent to the Main Activity

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -12,15 +12,12 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-    Button button3;
-
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
-        button3 = (Button) findViewById(R.id.button3);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,27 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("http://www.google.com");
-                Intent it = new Intent(Intent.ACTION_VIEW,uri);
-                startActivity(it);
-                finish();
-            }
-        });
 
     }
-//
-//    private void click(View view){
-//            //        Uri uri = Uri.parse("http://www.google.com");
-////        Intent it = new Intent(Intent.ACTION_VIEW,uri);
-////        startActivity(it);
-//        }
-
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        setContentView(R.layout.activity_main);
 }
 
--- app/src/main/java/com/example/loginapp/MainActivity2.java
+++ app/src/main/java/com/example/loginapp/MainActivity2.java
@@ -2,26 +2,48 @@ package com.example.loginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
     Button button2;
-
+    Button button5;
+    Button button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         button2 = (Button)findViewById(R.id.button2);
+        button5 = (Button)findViewById(R.id.button5);
+        button6 = (Button)findViewById(R.id.button6);
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent i = new Intent(MainActivity2.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity3.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+                    public void onClick(View view) {
+                Uri uri = Uri.parse("tel:8359838126");
+                Intent it = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(it);
+           }
         });
     }
 }
